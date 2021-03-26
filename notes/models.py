cdaed40d3da81cf3cdc84bf0bc9705a35d99be81
YurDavid <davidyevu18@gmail.com>
2021-03-26 19:21:49 +0000

    overided save method

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,14 @@
-from django.db import models
+from django.db import models, IntegrityError
 
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    
+
+    def save(self, *args, **kwargs):
+        if self.title != "" and self.content != "":
+            return super(Note, self).save(*args, **kwargs)
+        
+        # import pdb; pdb.set_trace()
+        raise IntegrityError
