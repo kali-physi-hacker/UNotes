d828fe049188f4f632f61a6b668c86002f8b22d8
Desmond Brown <browndesmond30@yahoo.com>
2021-03-26 18:42:58 +0000

    Added test for NoteSerializer

---
+++ notes/tests/test_serializers.py
@@ -0,0 +1,59 @@
+from django.test import TestCase 
+
+from notes.serializers import NoteSerializer
+
+
+class NoteSerializerTest(TestCase):
+	def setUp(self):
+		self.note_data = {
+			"title": "1st Church Service In March",
+			"content": "Holiness is staying away from porn"
+		}
+	
+	def test_validation_success(self):
+		"""
+		Tests that serializer.is_valid() is True if required fields and 
+		serializer.save() --> Saves note
+		returns:
+		"""
+		serializer = NoteSerializer(data=self.note_data)
+		
+		self.assertTrue(serializer.is_valid())
+		serializer_note = serializer.save()
+		for field in self.note_data:
+			self.assertEqual(exec(f"serializer_note.{field}"), self.note_data.get(field))
+		
+		self.assertTrue(hasattr(note, "date_created"))
+		
+		# Assert For model level
+		model_note = Note.objects.first()
+		for field in self.note_data:
+			self.assertEqual(exec(f"model_note.{field}"), self.note_data.get(field))
+		
+		self.assertEqual(serializer_note, model_note)
+
+	def test_validation_failure(self):
+		"""
+		Tests thata serializer.is_valid() if missing fields
+		returns:
+		"""
+		# missing title 
+		data = self.note_data.copy()
+		del data["title"]
+		
+		serializer = NoteSerializer(data=data)
+		self.assertFalse(serializer.is_valid())
+		with self.assertRaises(AssertionError):
+			serializer.save()
+
+		self.assertEqual(Note.objects.count(), 0)
+
+		data = self.note_data.copy()
+		del data["content"]
+
+		serializer = NoteSerializer(data=data)
+		self.assertFalse(serializer.is_valid()
+		with self.assertRaises(AssertionError):
+			serializer.save()
+
+		self.assertEqual(Note.objects.count(), 0)
