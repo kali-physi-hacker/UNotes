8ceb8bcd431fc736b45e143c9faa8a9898bcf2cf
YurDavid <davidyevu18@gmail.com>
2021-03-26 17:24:08 +0000

    defined Note model fields

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
