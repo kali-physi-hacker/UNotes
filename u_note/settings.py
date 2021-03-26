aff88d9e9a8790081b45d22b8c13b9b5921231ea
YurDavid <davidyevu18@gmail.com>
2021-03-26 17:33:42 +0000

    added notes to installed apps

--- u_note/settings.py
+++ u_note/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u*@*!s)zc3x15vc%ipb9$jlb6=o2r2ds6dbptiz#zrn8o7(pdp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes',
 ]
 
 MIDDLEWARE = [
