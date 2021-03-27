58ed2f7cc3d854ed00da18034ce322ea72e2ac80
Yevu David <davidyevu18@gmail.com>
2021-03-27 09:43:08 +0000

    defined a view function to post notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Note
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from .serializers import NoteSerializer
+from rest_framework import status
+
+
+@api_view(['POST'])
+def notes_lyric(request):
+    if request.method == "POST":
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
